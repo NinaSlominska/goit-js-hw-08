56ff3a2af8574e80f47c5ee518c2ba1123a0435f
NinaSlominska <nusya.slominskaya@gmail.com>
2022-12-25 18:38:17 +0200

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+// localStorage.setItem("settings", JSON.stringify(settings));
+
+import Player from "@vimeo/player";
+const iframe = document.querySelector('#vimeo-player');
+const myPlayer = new Player(iframe);
+myPlayer.on('timeupdate', onTime)
+if (localStorage.getItem("videoplayer-current-time")) {
+    myPlayer.setCurrentTime(localStorage.getItem("videoplayer-current-time"))
+}
+function onTime({seconds}){
+localStorage.setItem("videoplayer-current-time", JSON.stringify(seconds));
+   }
 
