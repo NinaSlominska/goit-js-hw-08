3c540e63158bf98ce17cc0efe7e6aa0e853bda92
NinaSlominska <nusya.slominskaya@gmail.com>
2022-12-25 17:21:41 +0200

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+const gallery = document.querySelector('.gallery')
+const markupArray = galleryItems.map(({ preview, original, description }) =>
+{
+   
+   return ` <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+       src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`})
+const markup = markupArray.join('')
+gallery.insertAdjacentHTML("beforeend", markup)
 
-console.log(galleryItems);
+let galleryLightbox = new SimpleLightbox('.gallery a', { captionsData:'alt', captionDelay:250 });
+gallery.addEventListener('click', galleryClick)
+function galleryClick(event) {
+    event.preventDefault()
+    if (event.target.nodeName !== 'IMG') {
+        return
+    }
+
+}
